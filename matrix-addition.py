bd535b1336790e741f455cac1f263f62957034c8
TheQYD <emailqasim@gmail.com>
2022-02-27 15:28:52 -0500

    First commit.

---
+++ matrix-addition.py
@@ -0,0 +1,12 @@
+#!python3
+
+import numpy
+
+def matrix_addition(matrix_1, matrix_2):
+    return matrix_1 + matrix_2
+
+
+matrix_addition(matrix_1, matrix_2)
+
+matrix_1 = [1, 2, 3]
+matrix_2 = [2, 4, 6]
